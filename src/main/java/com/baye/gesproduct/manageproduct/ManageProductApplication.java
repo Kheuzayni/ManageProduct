4275b930c1e82249b86b8153d8227cd0fb9f649b
unknown <bayecheikhfall249@gmail.com>
2023-01-22 21:18:33 +0000

    First Product

---
+++ src/main/java/com/baye/gesproduct/manageproduct/ManageProductApplication.java
@@ -0,0 +1,13 @@
+package com.baye.gesproduct.manageproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageProductApplication.class, args);
+    }
+
+}
