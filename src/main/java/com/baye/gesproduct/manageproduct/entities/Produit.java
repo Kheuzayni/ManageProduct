bbf09018e49813e4844dde0a0b920c01fb9b1ffe
unknown <bayecheikhfall249@gmail.com>
2023-01-25 19:32:27 +0000

    vla

---
+++ src/main/java/com/baye/gesproduct/manageproduct/entities/Produit.java
@@ -0,0 +1,20 @@
+package com.baye.gesproduct.manageproduct.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class Produit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProduit;
+    private String nomProduit;
+    private Double prixProduit;
+    private Date dateCreation;
+
+
+}
