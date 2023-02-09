0ef95434636b1799c73631882b7c39c81b227377
unknown <bayecheikhfall249@gmail.com>
2023-02-09 18:55:17 +0000

    Fix ProduitService and do ProduitService impl

--- src/main/java/com/baye/gesproduct/manageproduct/service/ProduitServiceImpl.java
+++ src/main/java/com/baye/gesproduct/manageproduct/service/ProduitServiceImpl.java
@@ -1,7 +1,34 @@
 package com.baye.gesproduct.manageproduct.service;
 
+import com.baye.gesproduct.manageproduct.entities.Produit;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ProduitServiceImpl implements ProduitService{
+public class ProduitServiceImpl extends ProduitService {
+
+    @Override
+    public Produit saveProduit(Produit P){
+        return null;
+    }
+
+    @Override
+    public Produit updateProduit(Produit P){
+        return null;
+    }
+
+    @Override
+    public void deleteProduit(Produit P){
+    }
+
+    public void deleteProduitById (Long id){}
+
+    public Produit getProduit(Long id){
+        return null;
+    }
+
+    public List<Produit> getAllProduits (){
+        return null;
+    }
 }
