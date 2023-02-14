84f684309dbf8255bd9a3b294f28be317eba5e43
unknown <bayecheikhfall249@gmail.com>
2023-02-14 21:04:35 +0000

    ProduitRepository

--- src/main/java/com/baye/gesproduct/manageproduct/repository/ProduitRepository.java
+++ src/main/java/com/baye/gesproduct/manageproduct/repository/ProduitRepository.java
@@ -3,6 +3,10 @@ package com.baye.gesproduct.manageproduct.repository;
 import com.baye.gesproduct.manageproduct.entities.Produit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 //Repository
 public interface ProduitRepository extends JpaRepository <Produit, Long> {
+    List<Produit> findByNomProduit(String nom);
+    List<Produit> findByNomProduitContains(String nom);
 }
