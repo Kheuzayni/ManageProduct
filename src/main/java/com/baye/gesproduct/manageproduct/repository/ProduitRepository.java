07e7f103f61c8c186ea4fc055aafcbe8247d0390
unknown <bayecheikhfall249@gmail.com>
2023-02-15 09:17:49 +0000

    requetes

--- src/main/java/com/baye/gesproduct/manageproduct/repository/ProduitRepository.java
+++ src/main/java/com/baye/gesproduct/manageproduct/repository/ProduitRepository.java
@@ -1,7 +1,10 @@
 package com.baye.gesproduct.manageproduct.repository;
 
+import com.baye.gesproduct.manageproduct.entities.Category;
 import com.baye.gesproduct.manageproduct.entities.Produit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,4 +12,20 @@ import java.util.List;
 public interface ProduitRepository extends JpaRepository <Produit, Long> {
     List<Produit> findByNomProduit(String nom);
     List<Produit> findByNomProduitContains(String nom);
+
+    /*@Query("select p from Produit p where p.nomProduit like %?1 and p.prixProduit > ?2")
+	List<Produit> findByNomPrix (String nom, Double prix);*/
+
+    @Query("select p from Produit p where p.nomProduit like %:nom and p.prixProduit > :prix")
+    List<Produit> findByNomPrix (@Param("nom") String nom, @Param("prix") Double prix);
+
+    @Query("select p from Produit p where p.categorie = ?1")
+    List<Produit> findByCategorie (Category category);
+
+    List<Produit> findByCategorieIdCat(Long id);
+
+    List<Produit> findByOrderByNomProduitAsc();
+
+    @Query("select p from Produit p order by p.nomProduit ASC, p.prixProduit DESC")
+    List<Produit> trierProduitsNomsPrix ();
 }
